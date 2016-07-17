8dbce0c2feaadef7285963122d2e6bf3b3dd4fea
cleriphil <cleri.phil@gmail.com>
2016-07-17 15:51:26 -0700

    create bar chart

--- createSVG.js
+++ createSVG.js
@@ -5,4 +5,21 @@ window.onload = function(){
               .attr('id', 'chart')
               .attr('width', w)
               .attr('height', h);
+
+  svg.selectAll('.bar')
+    .data(data) //bind data array
+    .enter() //in the enter phase
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', 0)
+      .attr('y', function(d,i){
+        //to stagger the bars
+        return i * 20;
+      })
+      .attr('width', function(d,i){
+        //d is data bound to element
+        //i is index of the data
+          return d;
+      })
+      .attr('height', 19); //height of each bar
 };
