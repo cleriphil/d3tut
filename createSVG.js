70bf3c93a4bfaec4a8266d84202baa01dc88ee4f
cleriphil <cleri.phil@gmail.com>
2016-07-17 18:17:34 -0700

    refactor and create plot function

--- createSVG.js
+++ createSVG.js
@@ -10,9 +10,12 @@ window.onload = function(){
               .attr('width', w)
               .attr('height', h);
 
-  svg.selectAll('.bar')
-    .data(data) //bind data array
-    .enter() //in the enter phase
+
+  function plot(params){
+    console.log(this)
+    this.selectAll('.bar')
+      .data(params.data) //bind data array
+      .enter() //in the enter phase
       .append('rect')
       .classed("bar", true)
       .attr('x', 0)
@@ -20,18 +23,42 @@ window.onload = function(){
         //to stagger the bars
         return y(i);
         //use y scaling function
-
       })
       .attr('width', function(d,i){
         //d is data bound to element
         //i is index of the data
-          return x(d);
-          //use x scaling function
+        return x(d);
+        //use x scaling function
       })
       .attr('height', function(d,i){
         return y(1)-1;
         //use y scaling function on first element and subtract 1 to add whitespace b/w bars
       }); //height of each bar
 
-      addText(svg);
+      this.selectAll('.bar-label')
+        .data(params.data)
+        .enter()
+          .append('text')
+          .classed('bar-label', true)
+          .attr('x', function(d,i){
+            return x(d); // x(d) is width of bar
+            //css used to adjust text position
+          })
+          .attr('dx', -4)
+          .attr('y', function(d,i){
+            return y(i);
+          })
+          .attr('dy', function(d,i){
+            return y(1)/1.5+2; //dy used to nudge text position to halfway point
+            // y(1) is height of one bar
+          })
+          .text(function(d,i){
+            return d; //display value
+          });
+  }
+  
+  plot.call(svg, {
+    data: data
+  }); //use .call to change this to svg instead of window
+
 };
